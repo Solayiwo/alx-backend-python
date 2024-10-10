e3ae813fd498731d570d9432507df4813c04601a
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-10-10 07:28:32 -0700

    modified

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Annotate the function's parameters and return type"""
 
-from typing import List, Tuple, Sequence
+from typing import Iterable, List, Tuple, Sequence
 
 
-def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Takes a list of sequences and returns a list of tuples.
     Each tuple contains an element from the list and its length.
