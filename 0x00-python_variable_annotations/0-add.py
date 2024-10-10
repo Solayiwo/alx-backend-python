61226c066144bfde9b20e9e70d3235b8caa18721
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-10-10 04:01:44 -0700

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Define the add function with type annotations"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that adds two floating-point numbers.
+
+    Args:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
