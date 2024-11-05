58db004f53daf88e572f36665d0e86d8efd6cc36
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-11-05 03:16:46 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,10 @@
 """Unittest module for utils module"""
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,3 +31,26 @@ class TestAccessNestedMap(unittest.TestCase):
 
         # Check if the missing key is in the exception message
         self.assertEqual(str(context.exception), f"'{missing_key}'")
+
+
+class TestGetJson(unittest.TestCase):
+    """A class that tests the get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test for the function to returns the expected output"""
+        with patch("utils.requests.get") as mock_get:
+            # Create a Mock response object with a .json() method
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            # Call get_json and check its return value
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+
+            # Check that requests.get was called once with test_url
+            mock_get.assert_called_once_with(test_url)
